2457a0e381e28d18630fb13a9a3e95d679abf621
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2024-11-04 15:36:54 +0530

    prog05_Unconditional Jumping added

--- prog04/prog04.asm
+++ prog04/prog04.asm
@@ -1,57 +1,63 @@
-
 section .data  
-        msgl db 0xa , 0xb, "Enter Digit: "       
-        ; We can write it as following
-        ; msgl db 0xa, 0xb , "E", 'n', 't' 
+    msgl db 0xa, 0xb, "Enter Digit: "    ; Prompt message
+    msgl_len equ $ - msgl                 ; Length of the prompt message
+    msg2 db 0xa, 0xb, "Square Value is: " ; Result message
+    msg2_len equ $ - msg2                 ; Length of the result message
 
 section .bss   
-        inbuf resb 1                         
+    inbuf resb 1                          ; Buffer for user input
+    outbuf resb 2                         ; Buffer for output result
 
 section .text                     
+global _start                             ; Entry point
 
-global _start                       ; here the real program starts to execute
-                         
 _start:
-    
 
-    ;Read from User Input
-    mov eax, 3                      ; syscall number for sys_write (write)
-    mov ebx, 0                      ; file descriptor 1 is stdout
-    mov ecx, inbuf              ; pointer to the string
-    mov edx, 1                     ; length of the string
-    int 0x80                        ; make syscall
+    ; Write the msgl to stdout
+    mov eax, 4                            ; syscall number for sys_write (write)
+    mov ebx, 1                            ; file descriptor 1 is stdout
+    mov ecx, msgl                         ; pointer to the prompt message
+    mov edx, msgl_len                     ; length of the prompt message
+    int 0x80                              ; make syscall
 
-     ; Write the msgl to stdout
-    mov eax, 4                      ; syscall number for sys_write (write)
-    mov ebx, 1                      ; file descriptor 1 is stdout
-    mov ecx, msgl              ; pointer to the string
-    mov edx, 23                     ; length of the string
-    int 0x80                        ; make syscall
+    ; Read single digit input from user
+    mov eax, 3                            ; syscall number for sys_read (read)
+    mov ebx, 0                            ; file descriptor 0 is stdin
+    mov ecx, inbuf                        ; pointer to the input buffer
+    mov edx, 1                            ; length to read
+    int 0x80                              ; make syscall
 
-    mov al, byte[inbuf]             ; Here al = [inbuf] -> square bracket is set to value in that address
-    sub al, '0'                     ; al = al - 'o' 
-    mul al                          ; al = al*al          
-                 
-                      
-    int 0x80                        ; make syscall
+    ; Convert ASCII to integer
+    mov al, byte [inbuf]                  ; Load input character
+    sub al, '0'                           ; Convert ASCII to integer
 
+    ; Square the number
+    mov bl, al                            ; Copy al to bl
+    mul bl                                ; Multiply al * bl -> result in ax (al = lower byte, ah = upper byte)
+
+    ; Convert the result back to ASCII for output
+    mov bl, 10                            ; Divisor for decimal conversion
+    div bl                                ; Divide ax by 10, result: quotient in al, remainder in ah
+    add al, '0'                           ; Convert quotient to ASCII
+    mov [outbuf], al                      ; Store the first character in outbuf
+    add ah, '0'                           ; Convert remainder to ASCII
+    mov [outbuf + 1], ah                  ; Store the second character in outbuf
+
+    ; Write msg2 to stdout (Square Value is:)
+    mov eax, 4                            ; syscall number for sys_write (write)
+    mov ebx, 1                            ; file descriptor 1 is stdout
+    mov ecx, msg2                         ; pointer to the result message
+    mov edx, msg2_len                     ; length of the result message
+    int 0x80                              ; make syscall
+
+    ; Write the square result to stdout
+    mov eax, 4                            ; syscall number for sys_write (write)
+    mov ebx, 1                            ; file descriptor 1 is stdout
+    mov ecx, outbuf                       ; pointer to the output buffer
+    mov edx, 2                            ; length of the output (2 characters)
+    int 0x80                              ; make syscall
 
     ; Exit the program
-    mov eax, 1                      ; syscall number for sys_exit (exit)
-    mov ebx, 1                      ; exit code 0
-    int 0x80                        ; make syscall
-
-
-
-
-
-
-
-
-;nasm -f elf32 prog01.asm -o prog01.o  ;This is the file which creates the .asm file into object file
-;ld -m elf_i386 -s -o Prog01 prog01.o  ;This is the exucutable file 
-
-; compile time - > when the high level language is compiled into machine code 
-; Run time - > when the machine code is executed into the processor
-
-
+    mov eax, 1                            ; syscall number for sys_exit (exit)
+    xor ebx, ebx                          ; exit code 0
+    int 0x80                              ; make syscall
---
+++ prog05_Conditional_Instructions/prog05.asm
@@ -0,0 +1,49 @@
+; Conditional Instruction
+
+;cmp al,3                ; al - 3   it like substruction, and it compares but not return Value
+                        ; it is like a flag setter
+
+
+;jmp label       ; change or make next Instruction to be executed 
+                ; make the cpu executed the next instruction at this address in the label
+
+
+
+section .data                                 ; used to declare the initialized data or constant
+    hello_msg db 'Hello, World!' , 0          ; Declare a null-terminated string
+                              
+section .text                       ; here the program code exist     
+
+global _start                       ; here the real program starts to execute
+                         
+_start:
+
+jmp ENDPROG
+    
+    ; Write the hello_msg to stdout
+    mov eax, 4                      ; syscall number for sys_write (write)
+    mov ebx, 1                      ; file descriptor 1 is stdout
+    mov ecx, hello_msg              ; pointer to the string
+    mov edx, 30                     ; length of the string
+    int 0x80                        ; make syscall
+
+ENDPROG:                            ; this is the address of the exit prompt
+    ; Exit the program
+    mov eax, 1                      ; syscall number for sys_exit (exit)
+    mov ebx, 1                      ; exit code 0
+    int 0x80                        ; make syscall
+
+
+
+
+
+
+
+
+;nasm -f elf32 prog01.asm -o prog01.o  ;This is the file which creates the .asm file into object file
+;ld -m elf_i386 -s -o Prog01 prog01.o  ;This is the exucutable file 
+
+; compile time - > when the high level language is compiled into machine code 
+; Run time - > when the machine code is executed into the processor
+
+
